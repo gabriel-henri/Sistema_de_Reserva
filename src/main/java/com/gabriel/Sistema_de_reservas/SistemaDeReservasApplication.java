35050adcbb639492f235c8a0c11cb3d97b33f072
gabriel-henri <gabrielhra00@gmail.com>
2025-05-06 12:13:35 -0300

    Primeiro commit

---
+++ src/main/java/com/gabriel/Sistema_de_reservas/SistemaDeReservasApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.Sistema_de_reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeReservasApplication.class, args);
+	}
+
+}
